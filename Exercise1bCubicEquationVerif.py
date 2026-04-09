7e7a6f19d4722895f0a67424fa8e8f894779c539
ShrohanMohapatra <46818279+ShrohanMohapatra@users.noreply.github.com>
2026-04-09 14:32:48 -0400

    Add files via upload

---
+++ Exercise1bCubicEquationVerif.py
@@ -0,0 +1,14 @@
+# Exercise1bCubicEquationVerif.py
+
+import unittest
+from sympy import *
+
+class test_Exercise1bCubicEquationVerif(unittest.TestCase):
+    def defaultTestCase1(self):
+        self.assertTrue(True)
+    def defaultTestCase2(self):
+        self.assertFalse(False)
+
+if __name__ == "__main__":
+    unittest.main()
+    
